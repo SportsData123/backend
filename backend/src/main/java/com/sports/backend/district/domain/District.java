f3ec47bd931feae6b805af1eab26f460c7a84ae7
soyeon1806 <ksy1118s@naver.com>
2024-11-19 12:40:55 +0900

    District Entity Class

---
+++ backend/src/main/java/com/sports/backend/district/domain/District.java
@@ -0,0 +1,35 @@
+package com.sports.backend.district.domain;
+
+import com.sports.backend.city.domain.City;
+import com.sports.backend.facility.domain.Facility;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "district")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class District {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long districtId; // 지역 ID (자동 증가)
+
+    @Column(nullable = false, length = 20)
+    private String districtName; // 지역명
+
+    @Column(nullable = false, length = 3, unique = true)
+    private String districtCode; // 지역 코드
+
+    @ManyToOne
+    @JoinColumn(name = "city_code", referencedColumnName = "cityCode", nullable = false)
+    private City city; // 지역이 속한 도시
+
+    @OneToMany(mappedBy = "district", cascade = CascadeType.ALL)
+    private List<Facility> facilities; // 지역과 연결된 시설 목록
+}
