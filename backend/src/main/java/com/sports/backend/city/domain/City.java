b07214077106051d914c853c480a34fa13c3d80d
soyeon1806 <ksy1118s@naver.com>
2024-11-19 12:40:46 +0900

    City Entity Class

---
+++ backend/src/main/java/com/sports/backend/city/domain/City.java
@@ -0,0 +1,33 @@
+package com.sports.backend.city.domain;
+
+import com.sports.backend.district.domain.District;
+import com.sports.backend.facility.domain.Facility;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "city")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cityId; // 도시 ID (자동 증가)
+
+    @Column(nullable = false, length = 10, unique = true)
+    private String cityName; // 도시명
+
+    @Column(nullable = false, length = 2, unique = true)
+    private String cityCode; // 도시 코드
+
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL)
+    private List<District> districts; // 도시와 연결된 지역 목록
+
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL)
+    private List<Facility> facilities; // 도시와 연결된 시설 목록
+}
