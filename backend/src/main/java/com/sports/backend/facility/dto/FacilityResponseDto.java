02558bc3f4eca99b7c1fad6e125ed6e2d68cd6b1
soyeon <ksy1118s@naver.com>
2024-11-20 18:30:13 +0900

    feat: add DTO for returning facility data in API responses

--- backend/src/main/java/com/sports/backend/facility/dto/FacilityResponseDto.java
+++ backend/src/main/java/com/sports/backend/facility/dto/FacilityResponseDto.java
@@ -1,4 +1,17 @@
 package com.sports.backend.facility.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class FacilityResponseDto {
+    private int facilityId;
+    private String facilityName;
+    private String facilityType;
+    private String facilityStatus;
+    private String roadAddress;
+    private double latitude;
+    private double longitude;
+    private char isAccessibleForDisabled;
 }
