cd6ca3b1b7b7a371a32b0625892527736a0161a8
soyeon <ksy1118s@naver.com>
2024-11-21 00:49:48 +0900

    feat: add City entity class

--- backend/src/main/java/com/sports/backend/city/domain/City.java
+++ backend/src/main/java/com/sports/backend/city/domain/City.java
@@ -17,12 +17,13 @@ import java.util.List;
 public class City {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long cityId; // 도시 ID (자동 증가)
+    @Column(name = "city_id")
+    private int cityId; // 도시 ID (자동 증가)
 
-    @Column(nullable = false, length = 10, unique = true)
+    @Column(name = "city_name", nullable = false, length = 10, unique = true)
     private String cityName; // 도시명
 
-    @Column(nullable = false, length = 2, unique = true)
+    @Column(name = "city_code", nullable = false, length = 2, unique = true)
     private String cityCode; // 도시 코드
 
     @OneToMany(mappedBy = "city", cascade = CascadeType.REMOVE)
